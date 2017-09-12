3fa491c89068f4349979487124232bfe90c09006
Jaime Gomez <jaimegdpr@users.noreply.github.com>
2017-09-12 13:19:41 +0200

    Setting dependencies

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,11 @@
 class ypbind {
+
     include ypbind::install
     include ypbind::service
     include ypbind::config
+
+    Class['ypbind::install'] ->
+    Class['ypbind::service'] ->
+    Class['ypbind::config']
+
 }
